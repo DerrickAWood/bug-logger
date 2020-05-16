d44b214261fa322e1ab38b00e5d02c27f0612a6e
Derrick A Wood <dwood_1994@yahoo.com>
2020-05-15 21:35:06 -0600

    log in to report

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home text-center">
     <h1>Welcome to Bug Logger</h1>
+    <h3 v-if="!$auth.isAuthenticated">Please Login to Report a Bug</h3>
     <create-bug></create-bug>
       <div class=" m-2" v-for="bug in bugs" :key="bug.id">
       <router-link
--- clientsrc/src/components/CreateBug.vue
+++ clientsrc/src/components/CreateBug.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <form @submit.prevent="addBug()">
+    <form v-if="$auth.isAuthenticated" @submit.prevent="addBug()">
       <input class="m-2" type="text" placeholder="whats the bug" v-model="newBug.title" required />
       <input type="text" placeholder="description" v-model="newBug.description" />
       <button class="btn btn-sm btn-primary m-2" type="submit">Post Bug</button>
@@ -25,10 +25,9 @@ export default {
     }
   },
   methods: {
-    // FIXME fix router 
     addBug(bugId) {
-      this.$store.dispatch("addBug", this.newBug);
-      this.newBoard = { title: "", description: "" };
+        this.$store.dispatch("addBug", this.newBug);
+        this.newBoard = { title: "", description: "" };
     }
   }
 };
